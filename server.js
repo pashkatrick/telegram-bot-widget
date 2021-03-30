a3c3fc82955e33917a1e9f0650d769de40610adc
pashkatrick <coolbatmanarrow@gmail.com>
2021-03-30 18:26:39 +0300

    [master] init commit: add dotenv requirenment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+require('dotenv').config()
 
 app.use(express.static('dist', {index: 'demo.html', maxage: '4h'}));
 app.use(bodyParser.json());
